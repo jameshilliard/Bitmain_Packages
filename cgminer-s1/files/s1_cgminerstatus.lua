47f63937ee766c13a881c9c2615678447f25d453
L. D. Pinney <ldpinney@gmail.com>
2015-10-14 00:50:35 -0500

    seperate cgminer s1 and s3 add r1

---
+++ cgminer-s1/files/s1_cgminerstatus.lua
@@ -0,0 +1,71 @@
+--[[
+usr/lib/lua/luci/model/cbi/cgminer/cgminerstatus.lua
+
+LuCI - Lua Configuration Interface
+
+Copyright 2015 L. D. Pinney <ldpinney@gmail.com>
+Copyright 2015 Andrew Smith
+Copyright 2013 Xiangfu
+Copyright 2008 Steven Barth <steven@midlink.org>
+Copyright 2008 Jo-Philipp Wich <xm@leipzig.freifunk.net>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+f = SimpleForm("cgminerstatus", translate("Miner Status"))
+f.reset = false
+f.submit = false
+
+t = f:section(Table, luci.controller.cgminer.summary(), translate("Summary"))
+t:option(DummyValue, "Elapsed", translate("Elapsed"))
+t:option(DummyValue, "GHS paid", translate("GH/S(paid)"))
+t:option(DummyValue, "GHS 5s", translate("GH/S(5s)"))
+t:option(DummyValue, "GHS 5m", translate("GH/S(5m)"))
+t:option(DummyValue, "GHS 15m", translate("GH/S(15m)"))
+t:option(DummyValue, "GHS av", translate("GH/S(avg)"))
+t:option(DummyValue, "Found Blocks", translate("FoundBlocks"))
+t:option(DummyValue, "Hardware Errors", translate("HW"))
+t:option(DummyValue, "Device Hardware%", translate("HW%"))
+t:option(DummyValue, "Utility", translate("Utility"))
+t:option(DummyValue, "Work Utility", translate("WU"))
+t:option(DummyValue, "Best Share", translate("BestShare"))
+
+t1 = f:section(Table, luci.controller.cgminer.pools(), translate("Pools"))
+t1:option(DummyValue, "POOL", translate("Pool"))
+t1:option(DummyValue, "URL", translate("URL"))
+t1:option(DummyValue, "User", translate("User"))
+t1:option(DummyValue, "Status", translate("Status"))
+t1:option(DummyValue, "Getworks", translate("GetWorks"))
+t1:option(DummyValue, "Priority", translate("Priority"))
+t1:option(DummyValue, "Difficulty Accepted", translate("DiffA#"))              
+t1:option(DummyValue, "Difficulty Rejected", translate("DiffR#"))              
+t1:option(DummyValue, "Difficulty Stale", translate("DiffS#"))
+t1:option(DummyValue, "Diff1 Shares", translate("Diff1#"))          
+t1:option(DummyValue, "Last Share Difficulty", translate("LSDiff"))
+t1:option(DummyValue, "Last Share Time", translate("LSTime"))
+t1:option(DummyValue, "Accepted", translate("Accepted"))
+t1:option(DummyValue, "Rejected", translate("Rejected"))
+t1:option(DummyValue, "Stale", translate("Stale"))
+
+t2 = f:section(Table, luci.controller.cgminer.devs(), translate("AntMiner"))  
+t2:option(DummyValue, "chain", translate("Chain#") )
+t2:option(DummyValue, "asic", translate("ASIC#"))
+t2:option(DummyValue, "frequency", translate("Frequency"))
+t2:option(DummyValue, "temp", translate("Temp"))
+t2:option(DummyValue, "status", translate("ASIC status"))
+
+t3 = f:section(Table, luci.controller.cgminer.devs(), translate(""))
+t3:option(DummyValue, "timeout", translate("Timeout") )
+t3:option(DummyValue, "regdata", translate("RegData") )
+t3:option(DummyValue, "voltage", translate("Voltage") )
+t3:option(DummyValue, "opt_bitmain_beeper", translate("Beeper") )
+t3:option(DummyValue, "opt_bitmain_tempoverctrl", translate("TempControl") )
+t3:option(DummyValue, "opt_bitmain_workdelay", translate("WorkDelay") )
+t3:option(DummyValue, "fan1", translate("Fan") )
+
+return f
---
+++ cgminer-s3/files/s3_cgminer.lua
@@ -0,0 +1,80 @@
+m = Map("cgminer", translate("Configuration"), translate(""))
+
+conf = m:section(TypedSection, "cgminer", "")
+conf.anonymous = true
+conf.addremove = false
+
+conf:tab("default", translate("General Settings"))
+
+pool1url = conf:taboption("default", Value, "pool1url", translate("Pool 1"))
+pool1user = conf:taboption("default", Value, "pool1user", translate("Pool1 worker"))
+pool1pw = conf:taboption("default", Value, "pool1pw", translate("Pool1 password"))
+conf:taboption("default", DummyValue, "", translate(""))
+pool2url = conf:taboption("default", Value, "pool2url", translate("Pool 2"))
+pool2user = conf:taboption("default", Value, "pool2user", translate("Pool2 worker"))
+pool2pw = conf:taboption("default", Value, "pool2pw", translate("Pool2 password"))
+conf:taboption("default", DummyValue, "", translate(""))
+pool3url = conf:taboption("default", Value, "pool3url", translate("Pool 3"))
+pool3user = conf:taboption("default", Value, "pool3user", translate("Pool3 worker"))
+pool3pw = conf:taboption("default", Value, "pool3pw", translate("Pool3 password"))
+conf:taboption("default", DummyValue, "", translate(""))
+
+pb = conf:taboption("default", ListValue, "pool_balance", translate("Pool Balance(Default: Failover)"))
+pb.default = "  "
+pb:value("  ", translate("Failover"))
+pb:value("--balance", translate("Balance"))
+pb:value("--load-balance", translate("Load Balance"))
+
+pb = conf:taboption("default", ListValue, "bitmain_nobeeper", translate("Beeper ringing(Default: true)"))
+pb.default = "  "
+pb:value("  ", translate("true"))
+pb:value("--bitmain-nobeeper", translate("false"))
+
+pb = conf:taboption("default", ListValue, "bitmain_notempoverctrl", translate("Stop running when temprerature is over 80 degrees centigrade(Default: true)"))
+pb.default = "  "
+pb:value("  ", translate("true"))
+pb:value("--bitmain-notempoverctrl", translate("false"))
+
+conf:tab("advanced", translate("Advanced Settings"))
+pb = conf:taboption("advanced", ListValue, "freq", translate("Frequency")) 
+pb.default = "18:218.75:1106"
+pb:value("12:325:0c82", translate("325M"))
+pb:value("13:318.75:1906", translate("319M"))
+pb:value("13:312.5:0c02", translate("312M"))
+pb:value("13:306.25:1806", translate("306M"))
+pb:value("13:300:0b82", translate("300M"))
+pb:value("13:293.75:1706", translate("294M"))
+pb:value("14:287.5:0b02", translate("287M"))
+pb:value("14:281.25:1606", translate("281M"))
+pb:value("14:275:0a82", translate("275M"))
+pb:value("14:268.75:1506", translate("269M"))
+pb:value("15:262.5:0a02", translate("262M"))
+pb:value("15:256.25:1406", translate("256M"))
+pb:value("15:250:0982", translate("250M"))
+pb:value("16:243.75:1306", translate("244M"))
+pb:value("16:237.5:1286", translate("237M"))
+pb:value("17:231.25:1206", translate("231M"))
+pb:value("17:225:0882", translate("225M (S3+ default)"))
+pb:value("18:218.75:1106", translate("219M (S3 default)")) 
+pb:value("18:212.5:1086", translate("212M"))
+pb:value("19:206.25:1006", translate("206M"))
+pb:value("19:200:0782", translate("200M"))
+pb:value("19:196.88:1f07", translate("197M"))
+pb:value("20:193.75:0f03", translate("193M"))
+pb:value("21:181.25:0e83", translate("181M"))
+pb:value("22:175:0d83", translate("175M"))
+pb:value("24:162.5:0c83", translate("162M"))
+pb:value("25:156.25:0c03", translate("156M"))
+pb:value("26:150:0b83", translate("150M"))
+pb:value("27:143.75:1687", translate("144M"))
+pb:value("28:137.5:0a83", translate("137M"))
+pb:value("30:131.25:0a83", translate("131M"))
+pb:value("31:125:0983", translate("125M"))
+pb:value("35:118.75:0903", translate("119M"))
+pb:value("35:112.5:0883", translate("112M"))
+pb:value("37:106.25:0803", translate("106M"))
+pb:value("38:100:4f83", translate("100M"))
+  
+pb = conf:taboption("advanced", Value, "voltage", translate("voltage"),"Modify Voltage, Click Save &#38; Apply, and then re-power the miner.")
+pb.default = "0000"
+return m
